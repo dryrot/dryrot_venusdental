52e8f997053daea6e86f659b0f2bae46ba12e1cf
jiwon <jiwonhwang@i-bricks.co.kr>
2021-12-01 21:05:37 +0900

    [index, mindex] Desktop(4k) 반응형 양옆 너비 재조정, review preview 타이틀 실제 데이터에서 가져오게함

---
+++ .next/static/chunks/pages/mobileIndex-939215b71751f4e466ce.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[830],{98:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/mobileIndex",function(){return u(2055)}])}},function(n){n.O(0,[868,435,55,774,888,179],(function(){return _=98,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
