4fb45963c971455a51e73da07143fde131c60865
jiwonhwang <jiwonhwang@saramin.co.kr>
2025-12-29 16:03:07 +0900

    feat(venus): 빌드파일 커밋

--- .next/server/chunks/382.js
+++ .next/server/chunks/382.js
@@ -482,7 +482,7 @@ const RightButton = () => {
       // window.location.href='http://www.codingfactory.net/';
       window.open("https://blog.naver.com/hhhhsomi/", "_blank");
     } else if (flag === "youtube") {
-      window.open(" https://www.youtube.com/channel/UCMXzQE1LTyEPw3NrNoz5B8A/", "_blank");
+      window.open("https://www.youtube.com/@venustv4613", "_blank");
     }
   };
 
